fd5024f1876cd142b062570bd2393bd7b96faa92
LordMalacath <104316712+LordMalacath@users.noreply.github.com>
2022-11-08 11:49:01 +0200

    new update

--- 4tech-market/src/api/firebase/firebase.js
+++ 4tech-market/src/api/firebase/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAQEdh5y32S6bTlrrvGeRj6HvdJQAbTVQs",
-    authDomain: "thech-market.firebaseapp.com",
-    projectId: "thech-market",
-    storageBucket: "thech-market.appspot.com",
-    messagingSenderId: "835186389096",
-    appId: "1:835186389096:web:0823a3f49d903fcc048bdd"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 export const app = initializeApp(firebaseConfig);
--- 4tech-market/src/pages/main/MainPage.jsx
+++ 4tech-market/src/pages/main/MainPage.jsx
@@ -3,6 +3,11 @@ import './mainPage.scss'
 export default function MainPage() {
     return (
         <div style={{ flex: 5 }}>
+            <div className='main'>
+                <div className='main__title'>
+                    Explore the world
+                </div>
+            </div>
         </div>
     )
 };
